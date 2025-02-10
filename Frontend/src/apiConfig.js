7d9f3529d0c65cb379f91ebded276afa5601e8e3
surya <suryajoshi617@gmail.com>
2025-02-10 15:20:42 +0530

    added backend url

--- Frontend/src/apiConfig.js
+++ Frontend/src/apiConfig.js
@@ -1,1 +1,2 @@
-export const url="http://localhost:1000/"
+// export const url="http://localhost:1000/"
+export const url="https://ecommerce-sp-jet.vercel.app/"
--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -7,7 +7,6 @@ import { ProductReducer } from "./reducers/productReducer";
 import { sellerReducer } from "./reducers/sellerReducers";
 import { cartReducer } from "./reducers/cartReducer";
 import { orderReducer } from "./reducers/orderReducer";
-// import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -17,5 +16,4 @@ const rootReducer = combineReducers({
   cart:cartReducer,
   order:orderReducer,
 });
-// const middleware=[thunk,promise]
 export const store = createStore(rootReducer, applyMiddleware(thunk, promise));
