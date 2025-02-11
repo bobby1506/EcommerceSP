797580c9792986db4e4bf389a6bc7449e32450f9
Bobby <bobbyupreti1234@gmail.com>
2025-02-11 10:06:48 +0530

    revert domain cookie

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -28,14 +28,12 @@ const Home = ({ getStores, emptyOrderMsg, user, order }) => {
             secure: true, // Required for cross-origin cookies
             sameSite: "None", // Ensures cookies work across different domains
             path: "/",
-            domain: ".vercel.app",
           })
         : Cookies.set("authToken", loginToken, {
             expires: 7,
             secure: false,
             sameSite: "None",
             path: "/",
-            domain: ".vercel.app",
           });
 
     const usertoken = Cookies.get("authToken");
